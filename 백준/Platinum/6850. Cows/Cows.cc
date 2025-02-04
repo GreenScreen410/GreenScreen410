c355f1844edbf7de426a9bc10bbee1f14a9a5af0
Mingyu Jung <pauljjang410@gmail.com>
2025-02-04 19:31:45 +0900

    [Platinum V] Title: Cows, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Platinum/6850. Cows/Cows.cc
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Point {
+  long long x, y;
+};
+vector<Point> points;
+
+long long ccw(Point a, Point b, Point c) {
+  return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+}
+
+bool compare_y(Point a, Point b) {
+  if (a.y == b.y) return a.x < b.x;
+  else return a.y < b.y;
+}
+
+bool compare_ccw(Point a, Point b) {
+  long long cp = ccw(points[0], a, b);
+  if (cp == 0) {
+    if (a.y == b.y) return a.x < b.x;
+    return a.y < b.y;
+  }
+  return cp > 0;
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  points.resize(N);
+  for (int i = 0; i < N; i++) {
+    cin >> points[i].x >> points[i].y;
+  }
+  sort(points.begin(), points.end(), compare_y);
+  sort(points.begin() + 1, points.end(), compare_ccw);
+
+  vector<Point> convex_hull;
+  convex_hull.push_back(points[0]);
+  convex_hull.push_back(points[1]);
+
+  for (int i = 2; i < N; i++) {
+    while (convex_hull.size() >= 2) {
+      Point b = convex_hull.back();
+      convex_hull.pop_back();
+      Point a = convex_hull.back();
+      if (ccw(a, b, points[i]) > 0) {
+        convex_hull.push_back(b);
+        break;
+      }
+    }
+    convex_hull.push_back(points[i]);
+  }
+
+  double result = 0;
+  for (int i = 1; i < convex_hull.size() - 1; i++) {
+    result += abs(ccw(convex_hull[0], convex_hull[i], convex_hull[i + 1])) * 0.5;
+  }
+
+  cout << (int)result / 50;
+
+  return 0;
+}
---
+++ 백준/Platinum/6850. Cows/README.md
@@ -0,0 +1,30 @@
+# [Platinum V] Cows - 6850 
+
+[문제 링크](https://www.acmicpc.net/problem/6850) 
+
+### 성능 요약
+
+메모리: 2184 KB, 시간: 8 ms
+
+### 분류
+
+볼록 껍질, 기하학, 다각형의 넓이
+
+### 제출 일자
+
+2024년 5월 10일 12:27:49
+
+### 문제 설명
+
+<p>Your friend to the south is interested in building fences and turning plowshares into swords. In order to help with his overseas adventure, they are forced to save money on buying fence posts by using trees as fence posts wherever possible. Given the locations of some trees, you are to help farmers try to create the largest pasture that is possible. Not all the trees will need to be used.</p>
+
+<p>However, because you will oversee the construction of the pasture yourself, all the farmers want to know is how many cows they can put in the pasture. It is well known that a cow needs at least 50 square metres of pasture to survive.</p>
+
+### 입력 
+
+ <p>The first line of input contains a single integer, n (1 ≤ n ≤ 10000), containing the number of trees that grow on the available land. The next n lines contain the integer coordinates of each tree given as two integers x and y separated by one space (where −1000 ≤ x, y ≤ 1000). The integer coordinates correlate exactly to distance in metres (e.g., the distance between coordinate (10, 11) and (11, 11) is one metre).</p>
+
+### 출력 
+
+ <p>You are to output a single integer value, the number of cows that can survive on the largest field you can construct using the available trees.</p>
+
